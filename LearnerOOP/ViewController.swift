492ddea906bac9a710d23b48ec45678f19f4de07
Marcel Wira <marcelwira20@gmail.com>
2019-05-15 12:17:16 +0700

    Initial Commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Marcel W on 15/05/19.
+//  Copyright © 2019 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
